5f0cd3a5146b29ebd674b7bd95a9222e8f03609c
Adriana Morales <adrianasmoraleso@gmail.com>
2026-06-22 20:29:37 -0500

    chore/Configuracion inicial del proyecto

---
+++ src/main/java/org/sistemabibliografico/App.java
@@ -0,0 +1,10 @@
+package org.sistemabibliografico;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.print("Configuración inicial del proyecto");
+
+
+    }
+}
